c7374ea4b8f03d6e7492108f7fb4ef0ef4b86ffd
ARISTOKCRAT <u.negmatov@Gmail.com>
2020-05-19 01:33:03 +0500

    * now settings tab has own init function + fuzzy logic base file path setting * now about tab has own init function + about tab filled up w/ wids

--- tmp.py
+++ tmp.py
@@ -1,16 +1,23 @@
-import json
+# import json
+#
+# with open("out_data/fuzzy_logic.json") as f:
+#     data = json.load(f)
+#
+# print(data.keys())
+# for item in data['68'].items():
+#     print(item)
+#
+# for i in range(69):
+#     print(data[str(i)]['feature_name'], end='\t')
+#     print(data[str(i)]['intervals_count'], end='\t')
+#
+#     for interval in data[str(i)]['intervals']:
+#         print(f"[{interval[0]:.2f}..{interval[1]:.2f}]({interval[3]:.2f})K{interval[2]}, ", end="")
+#     print(f"\t{data[str(i)]['stability']:.2f}")
 
-with open("out_data/fuzzy_logic.json") as f:
-    data = json.load(f)
+a = {1,2,3,4,5,}
 
-print(data.keys())
-for item in data['68'].items():
-    print(item)
+b = ', '.join(str(e) for e in a)
+print(b)
 
-for i in range(69):
-    print(data[str(i)]['feature_name'], end='\t')
-    print(data[str(i)]['intervals_count'], end='\t')
 
-    for interval in data[str(i)]['intervals']:
-        print(f"[{interval[0]:.2f}..{interval[1]:.2f}]({interval[3]:.2f})K{interval[2]}, ", end="")
-    print(f"\t{data[str(i)]['stability']:.2f}")
